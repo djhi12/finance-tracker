b6c4ba08bc24620558d75f56502bc456a0ee0bff
djhi12 <ido20002@byui.edu>
2024-05-04 15:04:05 +0800

    updated

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for tracker project.
+URL configuration for webapp project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -14,9 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('', views.home, name='home'),
+    # Add this line for the about page
+    path('about/', views.about, name='about'),
 ]

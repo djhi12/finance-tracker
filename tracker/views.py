1963d21f24b6dcb15a09d1b0ff93403742d0bcef
djhi12 <ido20002@byui.edu>
2024-05-05 20:37:06 +0800

    updated

--- tracker/views.py
+++ tracker/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
+import pandas as pd
+import matplotlib.pyplot as plt
+
 
 def home(request):
     return render(request, 'home.html')
